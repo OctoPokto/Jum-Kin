c19f5a30256238a515a9130be412738d5c5edfb8
Anders Findal Jørgensen <fj.anders@gmail.com>
2025-08-19 14:04:51 +0200

    Cam progression now works horizontal and  project file is changed to 4.4

--- Scripts/CamProgression.gd
+++ Scripts/CamProgression.gd
@@ -6,18 +6,27 @@ var playerPos
 
 func _ready():
 	playerPos = player.position
-	print("CamPos:",position.y)
-	print("PlayerPos:",playerPos.y)
+	print("PlayerPos:","x:",playerPos.x,"y:",playerPos.y)
+	print("CamPos:","x:",position.x,"y:",position.y)
 
 func _process(delta):
 	playerPos = player.position
 	if position.y-240 > playerPos.y: #going up
 		position.y -= 480
-		print("PlayerPos:",playerPos.y)
-		print("CamPos:",position.y)
+		print("PlayerPos:","x:",playerPos.x,"y:",playerPos.y)
+		print("CamPos:","x:",position.x,"y:",position.y)
 		
 	if position.y+240 < playerPos.y: #going down
 		position.y += 480
-		print("PlayerPos:",playerPos.y)
-		print("CamPos:",position.y)
-	
+		print("PlayerPos:","x:",playerPos.x,"y:",playerPos.y)
+		print("CamPos:","x:",position.x,"y:",position.y)
+		
+	if position.x-320 > playerPos.x: #going left
+		position.x -=640
+		print("PlayerPos:","x:",playerPos.x,"y:",playerPos.y)
+		print("CamPos:","x:",position.x,"y:",position.y)
+
+	if position.x+320 < playerPos.x: #going right
+		position.x +=640
+		print("PlayerPos:","x:",playerPos.x,"y:",playerPos.y)
+		print("CamPos:","x:",position.x,"y:",position.y)
