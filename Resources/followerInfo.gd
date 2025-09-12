951f0a57bc998363b770d11eaccddd73455c9ac1
Anders Findal Jørgensen <fj.anders@gmail.com>
2025-09-12 14:04:37 +0200

    Added Follower info recource and wrote some dialogue

---
+++ Resources/followerInfo.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name  FollowerInfo 
+
+@export var characterName : String
+@export var colour : Color
+
+@export var dialogue : PackedStringArray
+@export var dialogueSuccess : PackedStringArray
+@export var dialogueFalling : PackedStringArray
