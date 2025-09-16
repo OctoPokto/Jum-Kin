fadf0c5aa58c75fcf55557ddde63d11bd68f1b96
Anders Findal Jørgensen <fj.anders@gmail.com>
2025-09-16 13:57:43 +0200

    Made world 2 and added variable recording what screen the player is on

--- Scripts/CamProgression.gd
+++ Scripts/CamProgression.gd
@@ -3,6 +3,8 @@ extends RemoteTransform2D
 @onready var camera_2d = $"../Camera2D" #Not really needed
 
 var playerPos
+var currentScreen : Vector2
+
 
 func _ready():
 	playerPos = player.position
@@ -13,20 +15,32 @@ func _process(delta):
 	playerPos = player.position
 	if position.y-240 > playerPos.y: #going up
 		position.y -= 480
-		print("PlayerPos:","x:",playerPos.x,"y:",playerPos.y)
-		print("CamPos:","x:",position.x,"y:",position.y)
+		currentScreen.y -= 1
+		#print("PlayerPos:","x:",playerPos.x,"y:",playerPos.y)
+		#print("CamPos:","x:",position.x,"y:",position.y)
+		print("Screen: ","x: ",currentScreen.x,"y: ", currentScreen.y)
 		
 	if position.y+240 < playerPos.y: #going down
 		position.y += 480
-		print("PlayerPos:","x:",playerPos.x,"y:",playerPos.y)
-		print("CamPos:","x:",position.x,"y:",position.y)
+		currentScreen.y += 1
+		#print("PlayerPos:","x:",playerPos.x,"y:",playerPos.y)
+		#print("CamPos:","x:",position.x,"y:",position.y)
+		print("Screen: ","x: ",currentScreen.x,"y: ", currentScreen.y)
+
 		
 	if position.x-320 > playerPos.x: #going left
 		position.x -=640
-		print("PlayerPos:","x:",playerPos.x,"y:",playerPos.y)
-		print("CamPos:","x:",position.x,"y:",position.y)
+		currentScreen.x -= 1
+		#print("PlayerPos:","x:",playerPos.x,"y:",playerPos.y)
+		#print("CamPos:","x:",position.x,"y:",position.y)
+		print("Screen: ","x: ",currentScreen.x,"y: ", currentScreen.y)
 
 	if position.x+320 < playerPos.x: #going right
 		position.x +=640
-		print("PlayerPos:","x:",playerPos.x,"y:",playerPos.y)
-		print("CamPos:","x:",position.x,"y:",position.y)
+		currentScreen.x += 1
+		#print("PlayerPos:","x:",playerPos.x,"y:",playerPos.y)
+		#print("CamPos:","x:",position.x,"y:",position.y)
+		print("Screen: ","x: ",currentScreen.x,"y: ", currentScreen.y)
+		
+func _getCurrentScreen():
+	return currentScreen
