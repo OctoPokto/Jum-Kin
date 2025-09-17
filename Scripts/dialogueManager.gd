fa4612744eaa8bb9b8203bceca5d0876c49a5ca9
Anders Findal Jørgensen <fj.anders@gmail.com>
2025-09-17 16:13:47 +0200

    Word 2 almost done ad some dialogue changes

--- Scripts/dialogueManager.gd
+++ Scripts/dialogueManager.gd
@@ -3,23 +3,29 @@ extends Control
 @onready var content = get_node("Content")
 @onready var typing_timer = get_node("TypingTimer") as Timer
 
-var target
+@onready var target: Vector2
 
 func _ready() -> void:
+	visible = false
 	await get_tree().create_timer(2.0).timeout
 	typing_timer.timeout.connect(_on_TypingTimer_timeout)
-	update_message("Oh it's recording now? What do you want me to say?")
+	update_message("Oh it's recording now? What do you want me to say?", target)
 	
-func update_message(message: String)-> void:
+func update_message(message: String, target: Vector2)-> void:
 	content.text = message
+	set_position(target)
 	await get_tree().create_timer(2.0).timeout
 	content.visible_characters = 0
-	print("No visible characters")
+	visible = true
 	typing_timer.start()
 	
+
 func _on_TypingTimer_timeout()->void:
 	if content.visible_characters < content.text.length():
 		content.visible_characters +=1
 		print("a character is displayed")
 	else:
 		typing_timer.stop()
+		await get_tree().create_timer(2.0).timeout
+		visible = false
+		content.text = ""
