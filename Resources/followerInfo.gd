54025d5b00dd7ff3ccfc6f13db057461419890e5
Anders Findal Jørgensen <fj.anders@gmail.com>
2025-09-17 11:05:24 +0200

    All my changes
    Follower re-arangement, added pickup dialogue to info.
    level gate is now scaled correctly. Small world change, world 2 half done.
    Door now correct model

--- Resources/followerInfo.gd
+++ Resources/followerInfo.gd
@@ -4,6 +4,7 @@ class_name  FollowerInfo
 @export var characterName : String
 @export var color : Color
 
+@export var pickupDialogue : String
 @export var dialogue : PackedStringArray
 @export var dialogueSuccess : PackedStringArray
 @export var dialogueFalling : PackedStringArray
