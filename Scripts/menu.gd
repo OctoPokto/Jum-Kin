ba03e2bd8ff6d3dd9f7e4ca4b9bba17f9dde12b8
Anders Findal Jørgensen <fj.Anders@gmail.com>
2026-02-11 16:08:18 +0100

    A bunch of new assets. lvl1-2 is borked, needs a redraw.

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -3,8 +3,13 @@ extends Control
 func toggle_visibility(object):
 	if object.visible == true:
 		object.visible = false
+		get_tree().paused = false
+		print(get_tree().paused)
+
 	else:
-		object.visible=true
+		object.visible = true
+		get_tree().paused = true
+		print(get_tree().paused)
 
 func _exit_game() -> void:
 	get_tree().quit()
